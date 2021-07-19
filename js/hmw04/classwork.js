e0a08c92193681c2af94d0d62034893d299b3b0e
Katales <Oleksiy.Slisenko@gmail.com>
2021-07-19 15:41:42 +0300

    [js lesson 04 / ] added homework

---
+++ js/hmw04/classwork.js
@@ -0,0 +1,398 @@
+// - створити функцію яка приймає масив та виводить його
+console.log('- створити функцію яка приймає масив та виводить його');
+
+const outpArr = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`[${i}] ${arr[i]}`)
+    }
+}
+
+arr1 = createArr('number', 7);
+outpArr(arr1);
+
+
+// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+console.log('- створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)');
+
+const minNum = function () {
+    return minNumOfArr(arguments);
+}
+
+console.log('arguments: 5, 4, 1');
+console.log('result: ', minNum(5, 4, 1));
+console.log('arguments: 6, 21, 3, 4, 5, 4, 19');
+console.log('result: ', minNum(6, 21, 3, 4, 5, 4, 19));
+
+
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+console.log('- створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)');
+
+const maxNum = function () {
+    return maxNumOfArr(arguments);
+}
+
+console.log('arguments: 5, 4, 1');
+console.log('result: ', maxNum(5, 4, 1));
+console.log('arguments: 6, 21, 3, 4, 5, 4, 19');
+console.log('result: ', maxNum(6, 21, 3, 4, 5, 4, 19));
+
+
+// - створити функцію яка повертає найбільше число з масиву
+console.log('- створити функцію яка повертає найбільше число з масиву\n');
+
+function maxNumOfArr(arr) {
+    let max = arr[0];
+    for (let el of arr) {
+        max = (el > max ? el : max);
+    }
+    return max;
+}
+
+testArr = createArr('number', 5);
+console.log('arguments:');
+outpArr(testArr)
+console.log('result: ', maxNumOfArr(testArr));
+
+
+// - створити функцію яка повертає найменьше число з масиву
+console.log('- створити функцію яка повертає найменьше число з масиву');
+
+function minNumOfArr(arr) {
+    let min = arr[0];
+    for (let el of arr) {
+        min = (el < min ? el : min);
+    }
+    return min;
+}
+
+testArr = createArr('number', 5);
+console.log('arguments:');
+outpArr(testArr)
+console.log('result: ', minNumOfArr(testArr));
+
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+console.log('- створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13');
+
+function sumOfArr(arr) {
+    let sum = 0;
+    for (let el of arr) {
+        sum += el;
+    }
+    return sum;
+}
+
+testArr = createArr('number', 3);
+console.log('arguments:');
+outpArr(testArr)
+console.log('result: ', sumOfArr(testArr));
+
+// - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+console.log('- створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.');
+
+function averageOfArr(arr) {
+    return sumOfArr(arr) / arr.length;
+}
+
+testArr = createArr('number', 3);
+console.log('arguments:');
+outpArr(testArr)
+console.log('result: ', averageOfArr(testArr));
+
+
+// - створити функцію яка заповнює масив рандомними числами
+console.log('- створити функцію яка заповнює масив рандомними числами');
+
+const fillArrRandNums = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = getRandInt(2);
+    }
+}
+
+testArr = [, , , ,];
+console.log('initial array length:', testArr.length);
+console.log('initial array:');
+outpArr(testArr);
+
+fillArrRandNums(testArr);
+
+console.log('result array :');
+outpArr(testArr);
+
+
+// - Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів
+console.log('- Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів');
+
+const getAllKeys = function (arr) {
+    let keys = [];
+    for (let el of arr) {
+        keys = keys.concat(Object.keys(el));
+    }
+    return keys;
+}
+
+testArr = [{name: 'Dima', age: 13}, {model: 'Camry'}];
+console.log('initial array:');
+console.log('  [{name: \'Dima\', age: 13}, {model: \'Camry\'}]');
+
+let keys = getAllKeys(testArr);
+
+console.log('result (array of keys) :');
+outpArr(keys);
+
+
+// - Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
+console.log('- Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів');
+
+const getAllValues = function (arr) {
+    let values = [];
+    for (let el of arr) {
+        for (let key of Object.keys(el)) {
+            values.push(el[key]);
+        }
+    }
+    return values;
+}
+
+testArr = [{name: 'Dima', age: 13}, {model: 'Camry'}];
+console.log('initial array:');
+console.log('  [{name: \'Dima\', age: 13}, {model: \'Camry\'}]');
+
+let values = getAllValues(testArr);
+
+console.log('result (array of values) :');
+outpArr(values);
+
+
+// - створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
+console.log('- створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.');
+
+const sum2vectors = function (arr1, arr2) {
+    let res = [];
+    for (let i = 0; i < arr1.length; i++) {
+        res[i] = arr1[i] + arr2[i];
+    }
+    return res;
+}
+
+vector1 = createArr('number', 5);
+vector2 = createArr('number', 5);
+console.log('Initial array #1 :');
+outpArr(vector1);
+console.log('Initial array #2 :');
+outpArr(vector2);
+
+console.log('resulted array :');
+outpArr(sum2vectors(vector1, vector2));
+
+
+// - Дан масив ['a', 'b', 'c']. Додайте йому в кінець елементи 1, 2, 3 за допомогою циклу.
+console.log('- Дан масив [\'a\', \'b\', \'c\']. Додайте йому в кінець елементи 1, 2, 3 за допомогою циклу.');
+
+arr = ['a', 'b', 'c'];
+for (let i = 1; i < 4; i++) {
+    arr.push(i);
+}
+
+console.log('result array :');
+outpArr(arr);
+
+
+// - Дан масив [1, 2, 3]. Зробіть з нього новий масив [3, 2, 1].
+console.log('- Дан масив [1, 2, 3]. Зробіть з нього новий масив [3, 2, 1].');
+
+arr = [1, 2, 3];
+arr1 = [];
+iterations = arr.length;
+for (let i = 0; i < iterations; i++) {
+    arr1.push(arr.pop());
+}
+
+console.log('new array :');
+outpArr(arr1);
+
+
+// - Дан масив [1, 2, 3]. Додайте йому в кінець елементи 4, 5, 6.
+console.log('- Дан масив [1, 2, 3]. Додайте йому в кінець елементи 4, 5, 6.');
+
+arr = [1, 2, 3];
+for (let i = 4; i < 7; i++) {
+    arr.push(i);
+}
+
+console.log('result array :');
+outpArr(arr);
+
+
+// - Дан масив [1, 2, 3]. Додайте йому в початок елементи 4, 5, 6.
+console.log('- Дан масив [1, 2, 3]. Додайте йому в початок елементи 4, 5, 6.');
+
+arr = [1, 2, 3];
+for (let i = 6; i > 3; i--) {
+    arr.unshift(i);
+}
+
+console.log('result array :');
+outpArr(arr);
+
+
+//
+//
+// - Дан масив [1, 2, 3, 4, 5]. Перетворіть масив в [4, 5].
+console.log('- Дан масив [1, 2, 3, 4, 5]. Перетворіть масив в [4, 5].');
+
+arr = [1, 2, 3, 4, 5];
+arr = arr.slice(3, 5);
+
+console.log('result array :');
+outpArr(arr);
+
+
+// - Дан масив [1, 2, 3, 4, 5]. Перетворіть масив в [1,2].
+console.log('- Дан масив [1, 2, 3, 4, 5]. Перетворіть масив в [1,2].');
+
+arr = [1, 2, 3, 4, 5];
+arr = arr.slice(0, 2);
+
+console.log('result array :');
+outpArr(arr);
+
+
+// - Дан масив [1, 2, 3, 4, 5]. Зробіть з нього масив [1, 2, 3, 'a', 'b', 'c'].
+console.log('- Дан масив [1, 2, 3, 4, 5]. Зробіть з нього масив [1, 2, 3, \'a\', \'b\', \'c\']');
+
+arr = [1, 2, 3, 4, 5];
+arr.splice(3, 2);
+for (let i = 97; i < 100; i++) {
+    arr.push(String.fromCharCode(i));
+}
+
+console.log('result array :');
+outpArr(arr);
+
+
+// - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+console.log('- Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.');
+
+arr = createArr('number', 10);
+console.log('Initial array :');
+outpArr(arr);
+
+for (let el of arr) {
+    if (!(el % 2)) {
+        console.log('even element: ', el);
+    }
+}
+
+
+// - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+console.log('- Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.');
+
+arr = createArr('number', 10);
+console.log('Initial array :');
+outpArr(arr);
+arr1 = [];
+
+for (let el of arr) {
+    arr1.push(el);
+}
+
+console.log('result array :');
+outpArr(arr1);
+
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+console.log('- Дано масив: [ \'a\', \'b\', \'c\'] . За допомогою циклу for зібрати всі букви в слово.');
+
+arr = ['a', 'b', 'c'];
+let wrd = '';
+
+for (let i = 0; i < arr.length; i++) {
+    wrd += arr[i];
+}
+
+console.log('result word : ', wrd);
+
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+console.log('- Дано масив: [ \'a\', \'b\', \'c\'] . За допомогою циклу while зібрати всі букви в слово.');
+
+arr = ['a', 'b', 'c'];
+wrd = '';
+
+let i = 0;
+while (i < arr.length) {
+    wrd += arr[i];
+    i++;
+}
+
+console.log('result word : ', wrd);
+
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+console.log('- Дано масив: [ \'a\', \'b\', \'c\'] . За допомогою циклу for of зібрати всі букви в слово.');
+
+arr = ['a', 'b', 'c'];
+wrd = '';
+
+for (let el of arr) {
+    wrd += el;
+}
+
+console.log('result word : ', wrd);
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for in зібрати всі букви в слово.
+console.log('- Дано масив: [ \'a\', \'b\', \'c\'] . За допомогою циклу for in зібрати всі букви в слово.');
+
+arr = ['a', 'b', 'c'];
+wrd = '';
+
+for (let key in arr) {
+    wrd += arr[key];
+}
+
+console.log('result word : ', wrd);
+
+
+// -----------------------------------------------------
+//      auxiliary functions
+// -----------------------------------------------------
+function getRandInt(digits) {
+    return Math.floor(Math.random() * Math.pow(10, digits));
+}
+
+function createArr(type, length) {
+// Creates 1-d array filled with random values
+// type (string) - type of content [ 'number' | 'string' | 'boolean' | 'mixed' ]
+// length (number) - number of array elements
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+        let typeNow = type;
+        if (type === 'mixed') {
+            let dice = getRandInt(1);
+            if (dice <= 3) {
+                typeNow = 'number';
+            } else if (dice <= 6) {
+                typeNow = 'string';
+            } else {
+                typeNow = 'boolean';
+            }
+        }
+        let dice2 = getRandInt(2);
+
+        switch (typeNow) {
+            case 'number':
+                arr[i] = dice2;
+                break;
+            case 'string':
+                arr[i] = 'string value #' + dice2;
+                break;
+            case 'boolean':
+                arr[i] = (dice2 > 49);
+                break;
+        }
+    }
+    return arr;
+}
+
+// -----------------------------------------------------
---
+++ js/hmw04/homework.js
@@ -0,0 +1,192 @@
+// - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+console.log('- створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.');
+function createIntArr100(length) {
+// Creates 1-d array filled with random int values [0..100[
+// length - number of array elements
+    let arr = [];
+
+    for (let i = 0; i < length; i++) {
+        arr[i] = getRandInt(2);
+    }
+
+    return arr;
+}
+
+
+// - Змінти попередню функцію, щоб діапазон можна було визначити через аргументи.
+console.log('- Змінти попередню функцію, щоб діапазон можна було визначити через аргументи.');
+function createIntArr(length, digits) {
+// Creates 1-d array filled with random values
+// length (number) - number of array elements
+// digits - number of digits in a randomly generated integer value
+    let arr = [];
+
+    for (let i = 0; i < length; i++) {
+        arr[i] = getRandInt(digits);
+    }
+
+    return arr;
+}
+
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+console.log('- створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort');
+
+let arr = createIntArr(7, 3);
+console.log('initial array:');
+outpArr(arr);
+
+let arrSorted = arr.sort( function (currEl, nextEl) {
+    return currEl - nextEl;
+});
+
+console.log('sorted array :');
+outpArr(arrSorted);
+
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, за лишивши тільки парні числа
+console.log('- створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, за лишивши тільки парні числа');
+
+arr = createIntArr(7, 3);
+console.log('initial array:');
+outpArr(arr);
+
+let arrFiltered = arr.filter( function (el) {
+    return el % 2 === 0;
+});
+
+console.log('filtered array :');
+outpArr(arrFiltered);
+
+// - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на срінгові.
+console.log('- створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об\'єкти в масиві на срінгові.');
+
+arr = createIntArr(7, 3);
+console.log('initial array:');
+outpArr(arr);
+
+let arrStr = arr.map( function (el) {
+    return el.toString();
+});
+
+console.log( 'array of strings :');
+outpArr(arrStr);
+
+
+// - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+console.log('- Створити функцію конструктор для об\'єктів User з полями id, name, surname , email, phone');
+
+class User {
+    id;
+    name;
+    surname;
+    email;
+    phone;
+
+    constructor(id, name, surname, email, phone) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+    }
+}
+
+
+// створити пустий масив, наповнити його 10 об'єктами new User(....)
+console.log('створити пустий масив, наповнити його 10 об\'єктами new User(....)');
+testArr = [
+    new User(getRandInt(3), 'Serge', 'Adf', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Alex', 'ZVD', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Igor', 'ADf', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Al', 'Dfa', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Steven', 'HGd', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Kirill', 'Eds', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Olga', 'Csd', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Roman', 'Wec', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Irina', 'Veb', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`),
+    new User(getRandInt(3), 'Zack', 'Zsa', `user${getRandInt(2)}@gml.com`, `+380 67 ${getRandInt(7)}`)
+];
+
+console.log( 'array of users :');
+// console.log(JSON.stringify(testArr));
+outpArr(testArr);
+
+// Взяти масив цей  User[] та: Відфільтрувати , залишивши тільки об'єкти з парними id (filter) та Відсортувати його по id. по зростанню (sort)
+console.log('Взяти масив цей  User[] та: Відфільтрувати , залишивши тільки об\'єкти з парними id (filter) та Відсортувати його по id. по зростанню (sort)');
+
+testArr = testArr.filter(function (el) {
+    return el.id % 2 === 0;
+})
+console.log('Filtered even IDs:');
+outpArr(testArr);
+
+testArr = testArr.sort( function(curr, next) {
+    return curr.id - next.id;
+});
+console.log('Sorted by ID:');
+outpArr(testArr);
+
+
+// -----------------------------------------------------
+//      auxiliary functions
+// -----------------------------------------------------
+function crLi(textLi, classesLi) {
+    return `<li class="${classesLi}"> ${textLi} </li>`
+}
+
+
+function getRandInt(digits) {
+    return Math.floor(Math.random() * Math.pow(10, digits));
+}
+
+
+function createArr(type, length, digits) {
+// Creates 1-d array filled with random values
+// type (string) - type of content [ 'number' | 'string' | 'boolean' | 'mixed' ]
+// length (number) - number of array elements
+// digits - number of digits in a randomly generated integer value
+    let arr = [];
+
+    for (let i = 0; i < length; i++) {
+        let typeNow = type;
+        if (typeNow === 'mixed') {
+            let dice = getRandInt(1);
+            if (dice <= 3) {
+                typeNow = 'number';
+            } else if (dice <= 6) {
+                typeNow = 'string';
+            } else {
+                typeNow = 'boolean';
+            }
+        }
+        let dice2 = getRandInt(digits);
+
+        switch (typeNow) {
+            case 'number':
+                arr[i] = dice2;
+                break;
+            case 'string':
+                arr[i] = 'string value #' + dice2;
+                break;
+            case 'boolean':
+                arr[i] = (dice > 4);
+                break;
+        }
+    }
+    return arr;
+}
+
+function outpArr(arr) {
+    if (typeof arr == 'object') {
+        for (let i = 0; i < arr.length; i++) {
+            console.log(JSON.stringify(arr[i]) );
+        }
+    }   else {
+        for (let i = 0; i < arr.length; i++) {
+            console.log(`[${i}]`, arr[i]);
+        }
+    }
+}
+
+// -----------------------------------------------------
