aefe07ad5cbc47265f4c309849d931678f4b7575
Katales <Oleksiy.Slisenko@gmail.com>
2021-08-06 04:32:19 +0300

    [js lesson 07 / homework]

--- js/hmw07/js/day-sched.js
+++ js/hmw07/js/day-sched.js
@@ -1,3 +1,105 @@
+// -------------------- Array of tasks ---------------------
+const tasks = [
+    {
+        name: 'Snooze the alarm!!!',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Get up!',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Make the bed',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Have a breakfast',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Do the dishes',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Dude, where is my car ?!?',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Commute to work',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Recall my UserID',
+        func: async function (name) {
+            console.log(`\t ${name}`);
+            console.log(`\t WTF is my UserID !!!????`);
+            const userId = prompt('Pls, enter my UserID:');
+            console.log('Oh, my Gosh ! My ID is', userId);
+        },
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Get comments on my post',
+        func: async function (name) {
+            console.log(`\t ${name}`);
+            await fetch(`https://jsonplaceholder.typicode.com/posts/2/comments`)
+                .then(resp => resp.json())
+                .then(comments => {
+                    for (const comment of comments) {
+                        console.log(`\t\tID:${comment.id} Comment:${comment.name}`);
+                    }
+                });
+        },
+        duration: getRandInt(1) / 2
+    },
+    {
+        name: 'Let\'s call it a day!',
+        func: logTaskName,
+        duration: getRandInt(1) / 2
+    }
+];
+// ---------------------------------------------------------
 
-const task1Screen = document.getElementById('task1');
 
+const firstNick = Date.now();
+
+let promisePtr;
+for (let i = 0; i < tasks.length; i++) {
+    promisePtr = queueTask(i, promisePtr);
+}
+
+function queueTask(taskNum, currentPromise) { //v.4.0
+    if (!currentPromise) {
+        return new Promise(function (resolve, reject) {
+            const task = tasks[taskNum];
+            console.log(`-> New task #${taskNum + 1} (${task.duration} sec) [timer=${timeFromStart()} sec.] ...`);
+            setTimeout(async () => {
+                await (task.func)(task.name);
+                console.log(`... FINISHED [timer=${timeFromStart()} sec.]`)
+                resolve(++taskNum);
+            }, task.duration * 1_000);
+        });
+    }
+
+    return currentPromise.then(taskNum => queueTask(taskNum));
+}
+
+
+
+// ======================================================================
+//      AUX FUNCTIONS
+//
+function timeFromStart() {
+    return ((Date.now() - firstNick) / 1_000);
+}
+
+function logTaskName (name) {
+    console.log(`\t ${name}`);
+}
--- js/hmw07/js/my_lib.js
+++ js/hmw07/js/my_lib.js
@@ -1,9 +1,20 @@
-const nameLS = 'products';
 
-function crEl (tag, id, classes, text) {
+function crEl(tag, id, classes, text) {
     const el = document.createElement(tag);
-    el.id = id || '';
+    id = (id || '').trim().split(' ')[0];
+    if (id) el.id = id;
     el.innerText = text || '';
-    el.classList = classes || '';
+    classes = strNormSpaces(classes || '');
+    if (classes) {
+        classes.split(' ').forEach(className => el.classList.add(className));
+    }
     return el;
+}
+
+function strNormSpaces(str) {
+    return str.replace(/\b\s+\b/g, ' ').trim();
+}
+
+function getRandInt(digits) {
+    return Math.floor(Math.random() * Math.pow(10, digits));
 }
