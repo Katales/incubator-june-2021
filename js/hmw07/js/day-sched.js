1b3ca02f0b31bf29f2589b1807f1f6ec20e5f236
Katales <Oleksiy.Slisenko@gmail.com>
2021-08-08 01:28:44 +0300

    [js lesson 07 / homework] HSKP2

--- js/hmw07/js/day-sched.js
+++ js/hmw07/js/day-sched.js
@@ -37,11 +37,11 @@ const tasks = [
     },
     {
         name: 'Recall my UserID',
-        func: async function (name) {
+        func: function (name) {
             console.log(`\t ${name}`);
             console.log(`\t WTF is my UserID !!!????`);
             const userId = prompt('Pls, enter my UserID:');
-            console.log('Oh, my Gosh ! My ID is', userId);
+            console.log('\t Oh, my Gosh ! My ID is', userId);
         },
         duration: getRandInt(1) / 2
     },
@@ -49,15 +49,13 @@ const tasks = [
         name: 'Get comments on my post',
         func: async function (name) {
             console.log(`\t ${name}`);
-            await fetch(`https://jsonplaceholder.typicode.com/posts/2/comments`)
-                .then(resp => resp.json())
-                .then(comments => {
-                    for (const comment of comments) {
-                        console.log(`\t\tID:${comment.id} Comment:${comment.name}`);
-                    }
-                });
+            let comments = await fetch(`https://jsonplaceholder.typicode.com/posts/2/comments`)
+                .then(resp => resp.json());
+            for (const comment of await comments) {
+                console.log(`\tID:${comment.id} Comment:${comment.name}`);
+            }
         },
-        duration: getRandInt(1) / 2
+        duration: 0 //getRandInt(1) / 2
     },
     {
         name: 'Let\'s call it a day!',
@@ -75,6 +73,7 @@ const tasks = [
 
 const firstNick = Date.now();
 
+//-------------------- METHOD 1 (Using promises) --------------------------
 let promisePtr;
 for (let i = 0; i < tasks.length; i++) {
     promisePtr = queueTask(i, promisePtr);
@@ -95,6 +94,32 @@ function queueTask(taskNum, currentPromise) {
 
     return currentPromise.then(taskNum => queueTask(taskNum));
 }
+//-------------------- METHOD 1 (Using promises) --------------------------
+
+
+
+// //-------------------- METHOD 2 (Using async/await) --------------------------
+//
+// main();
+//
+// async function main () {
+//     for (let i = 0; i < tasks.length; i++) {
+//         await launchTask(i);
+//     }
+// }
+//
+// function launchTask (taskNum) {
+//     return new Promise ( function (resolve, reject) {
+//         const task = tasks[taskNum];
+//         console.log(`-> New task #${taskNum + 1} (${task.duration} sec) [timer=${timeFromStart()} sec.] ...`);
+//         setTimeout(async () => {
+//             await (task.func)(task.name);
+//             console.log(`... FINISHED [timer=${timeFromStart()} sec.]`);
+//             resolve();
+//         }, task.duration * 1_00);
+//     })
+// }
+// //-------------------- METHOD 2 (Using async/await) --------------------------
 
 
 
