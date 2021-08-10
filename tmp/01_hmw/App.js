bcc5b3bd7e9ca09616b386c2ad9a96cd7d31eb72
Katales <Oleksiy.Slisenko@gmail.com>
2021-08-10 22:11:12 +0300

    [React lesson#01 / homework] final

---
+++ tmp/01_hmw/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './css/bem.css'
+import './css/common.css'
+// import Personage from './components/personages/personage/Personage.js';
+import {Personages} from './components/personages/Personages.js';
+
+function App() {
+  return (
+    <div className="taskScreen">
+      <Personages/>
+    </div>
+  );
+}
+
+export default App;
