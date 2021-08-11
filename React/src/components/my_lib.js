20ef0fb0d3c05dd7103508db6428544009a74698
Katales <Oleksiy.Slisenko@gmail.com>
2021-08-11 15:13:25 +0300

    [React_ORIGIN (clean)] v.1.

--- React/src/components/my_lib.js
+++ React/src/components/my_lib.js
@@ -1,15 +1,6 @@
 
-function crEl(tag, id, classes, text) {
-    const el = document.createElement(tag);
-    id = (id || '').trim().split(' ')[0];
-    if (id) el.id = id;
-    el.innerText = text || '';
-    classes = strNormSpaces(classes || '');
-    if (classes) {
-        classes.split(' ').forEach(className => el.classList.add(className));
-    }
-    return el;
-}
+
+export {strNormSpaces, getRandInt};
 
 function strNormSpaces(str) {
     return str.replace(/\b\s+\b/g, ' ').trim();
@@ -18,3 +9,4 @@ function strNormSpaces(str) {
 function getRandInt(digits) {
     return Math.floor(Math.random() * Math.pow(10, digits));
 }
+
