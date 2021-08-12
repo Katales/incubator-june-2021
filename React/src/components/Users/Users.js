979abc9adb670a57e59cdc0547fcf3d86a484448
Katales <Oleksiy.Slisenko@gmail.com>
2021-08-12 21:39:40 +0300

    [React lesson#2 / homework] v.1.

--- React/src/components/Users/Users.js
+++ React/src/components/Users/Users.js
@@ -5,7 +5,7 @@ import {getUser, getUsers} from "../../services/user.services";
 import {User} from "./User/User"
 
 export function Users() {
-    let [user, setUserN] = useState([]);
+    let [userN, setUserN] = useState([]);
     let [users, setUsers] = useState([]);
     useEffect(() => {
         getUser(1).then(user => {
@@ -18,7 +18,8 @@ export function Users() {
     return (
         <div id={'users'} className={'users flxCol'}>
             {/*<p> One user: (to test <b>getUser(id)</b>)</p>*/}
-            {/*<User user={user}/>*/}
+            {/*/!*<User user={user}/>*!/ this line doesn't work - WTF ?!?*/}
+            {/*{[userN].map(user => <User user={user} key={user.id}/>)}*/}
             {/*<hr/>*/}
             {/*<p>ALL users:</p>*/}
             {users.map(user => <User user={user} key={user.id}/>)}
