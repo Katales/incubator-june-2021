82ace19d744a7cef5d1c8999526e383bc66b4aae
Katales <Oleksiy.Slisenko@gmail.com>
2021-09-30 21:32:29 +0300

    [NodeJS lesson 01/ classwork]

--- NodeJS/01_clw/01_clw.js
+++ NodeJS/01_clw/01_clw.js
@@ -1,5 +1,6 @@
 /*
-- у вас є масив юзрів (до 10), з такими полями наприклад - const users = [
+- у вас є масив юзерів (до 10), з такими полями наприклад -
+const users = [
     { name: 'olya', gender: 'female', age: 20 }
         ...
 ],
@@ -9,3 +10,43 @@
 і розподілити ваших юзерів саме по відповідних папках
 
  */
+
+const people = [
+    { name: 'Mata', gender: 'female', age: 34 },
+    { name: 'Arsen', gender: 'male', age: 17 },
+    { name: 'Venik', gender: 'male', age: 67 },
+    { name: 'Gala', gender: 'female', age: 24 },
+    { name: 'Aisha', gender: 'female', age: 31 },
+    { name: 'Afrikan', gender: 'male', age: 37 },
+    { name: 'Helga', gender: 'female', age: 19 },
+    { name: 'Bogdana', gender: 'female', age: 25 },
+    { name: 'Maxin', gender: 'female', age: 29 },
+    { name: 'Michaela', gender: 'female', age: 20 },
+    { name: 'Konstantyn', gender: 'male', age: 23 },
+    { name: 'Yurko', gender: 'male', age: 20 }
+]
+
+const fs = require('fs');
+const path = require('path')
+
+const basePath = __dirname;
+const filesDir = 'files';
+const maleStemDir = 'men';
+const femaleStemDir = 'women';
+// const inputFile =  'people.json'
+const ageBnd = 20;
+let personFileFnm;
+
+// const inputFileFnm = path.join(basePath, filesDir, inputFile);
+// const menBDir = path.join(basePath, filesDir, maleStemDir+'Below'+ageBnd);
+// const menODir = path.join(basePath, filesDir, maleStemDir+'Over'+ageBnd);
+// const wmnBDir = path.join(basePath, filesDir, femaleStemDir+'Below'+ageBnd);
+// const wmnODir = path.join(basePath, filesDir, femaleStemDir+'Over'+ageBnd);
+
+for (const person of people) {
+    personFileFnm =   ( (person.gender === 'male') ? maleStemDir : femaleStemDir)
+                    + ( (person.age < ageBnd) ? 'Below' : 'Over') + ageBnd;
+    personFileFnm = path.join(basePath, filesDir, personFileFnm, person.name + '.json');
+    console.log (personFileFnm, person);
+}
+
