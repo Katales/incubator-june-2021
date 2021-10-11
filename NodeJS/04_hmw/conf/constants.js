10b3d81b56ff81f6eaaa8c5b212bb1b814be491b
Katales <Oleksiy.Slisenko@gmail.com>
2021-10-11 19:20:22 +0300

    [NodeJS lesson 04/ homework]

--- NodeJS/04_hmw/conf/constants.js
+++ NodeJS/04_hmw/conf/constants.js
@@ -1,12 +1,8 @@
 module.exports = {
     MONGO_CONNECT_URL: 'mongodb://localhost:27017/Ndjs-04',
     APP_PORT: 5000,
+
     userdom:  {
-        INTERNAL: 'INTERNAL',
-        RETAIL: 'RETAIL',
-        B2B: 'B2B'
-    },
-    userdomain:  {
         INTERNAL: 'INTERNAL',
         RETAIL: 'RETAIL',
         B2B: 'B2B'
