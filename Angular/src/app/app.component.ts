b48cc194f0f82fd6a6eb420d99df636404e89126
Katales <Oleksiy.Slisenko@gmail.com>
2021-11-16 19:09:30 +0200

    [Angular MASTER]

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log('>> ngOnInit started');
+  }
+
 }
